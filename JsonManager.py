3546bf5415e3b89d157f4a890e78d1567f4b1578
Enderman9424 <ruzgar.cinar0601@gmail.com>
2026-01-20 22:15:22 +0300

    Add new json manager for new test structure

---
+++ JsonManager.py
@@ -0,0 +1,75 @@
+import json
+import os
+
+class Testmanager:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        self.tests = []
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        
+    def get_tests(self):
+        self.tests = {}
+
+        for filename in os.listdir(self.folder_path):
+            if filename.endswith(".json"):
+                file_path = os.path.join(self.folder_path, filename)
+
+                with open(file_path, "r", encoding="utf-8") as file:
+                    try:
+                        data = json.load(file)
+
+                        # JSON içinden alınan değerler (sen ayarlarsın)
+                        exam_name = data.get("exam_name")
+                        exam_description = data.get("exam_description")
+                        lesson_name = data.get("lesson_name")
+
+                        # key: dosya adı, value: liste
+                        self.tests[filename] = [
+                            exam_name,
+                            exam_description,
+                            lesson_name
+                        ]
+
+                    except json.JSONDecodeError:
+                        print(f"JSON okunamadı: {file_path}")
+
+        return self.tests
+    
+    def create_test(self, exam_name, exam_description, lesson_name, exam_data:dict):
+        
+        with open(os.path.join(self.folder_path, f"{exam_name}.json"), "w", encoding="utf-8") as file:
+            data = {
+                "exam_name": exam_name,
+                "exam_description": exam_description,
+                "lesson_name": lesson_name,
+                "exam_data": exam_data
+            }
+            #Exampe and structure
+            #data = {
+            #        "exam_name": exam_name,
+            #        "exam_description": exam_description,
+            #        "lesson_name": lesson_name,
+            #        "exam_data": [
+            #           {
+            #                "question": "question",
+            #                "options": ["A", "B", "C", "D"],
+            #                "correct": index of correct option
+            #            },
+            #            {
+            #                "question": "questions",
+            #                "options": ["A", "B", "C", "D"],
+            #                "correct": index of correct option
+            #            }
+            #        ]
+            #    }
+
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+    def delete_test(self, filename):
+        file_path = os.path.join(self.folder_path, filename)
+        try:
+            os.remove(file_path)
+            print(f"{filename} başarıyla silindi.")
+        except FileNotFoundError:
+            print(f"{filename} bulunamadı.")
